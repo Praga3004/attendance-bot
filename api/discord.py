e7aebf4cf6b8e23e7695e7ac30e4b6f3f422bc45
Praga3004 <pragadeswaran.m.s@gmail.com>
2025-11-01 15:28:17 +0530

    reverting to successfull deployment

--- api/discord.py
+++ api/discord.py
@@ -12,8 +12,7 @@ import logging
 import pytz
 
 # Discord signature verification
-# import nacl.signing
-
+import nacl
 # Google APIs
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
